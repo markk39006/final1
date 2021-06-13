e9eb5d75ab311e9a7648939e88e2773d9bb6e1fe
markk39006 <markk39006@gmail.com>
2021-06-13 16:52:50 +0700

    1

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] boytest = new int[3, 3];
+            for (int i = 0; i <= 2; i++)
+            {
+                for(int j = 0; j <= 2; j++)
+                {
+                    boytest[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i <= 2; i++)
+            {
+                for (int j = 0; j <= 2; j++)
+                {
+                    Console.Write(boytest[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
